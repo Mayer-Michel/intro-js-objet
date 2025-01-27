2f6e6e09550a8697ba7b64ba9a35638410261e0e
Mayer MICHEL <mayer.mohib@gmail.com>
2025-01-27 14:46:41 +0100

    Héritage de classe

--- app/script.js
+++ app/script.js
@@ -5,12 +5,46 @@ class Character {
     // Déclaration de propriétés publiques
     hp;
     armor;
+    luck;
+
+    // Constructeur
+    constructor( jsonCharacter ) {
+        this.hp = jsonCharacter.hp;
+        this.armor = jsonCharacter.armor;
+        this.luck = jsonCharacter.luck;
+    }
 
     // Déclaration de méthode publique
-    waltk() {
+    walk() {
         console.log('Je marche')
     }
 }
 
-let gandalf = new Character();
-console.log( gandalf );
+class Thief extends Character {
+
+    constructor(jsonThief){
+        // super() est le constructeur du parent (Character)
+        // En JS on doit obligatoirement l'appeler en premier dans la constructeur enfant
+        super(jsonThief);
+        this.hp = jsonThief.hp;
+        this.armor = jsonThief.armor;
+        this.luck = jsonThief.luck * 1.5;
+    }
+    picklock() {
+        console.log( 'Crochetage en cours ...' );
+    }
+
+    // On peut surcharger une méthode parente pour redéfinir totalement son fonctionnement
+    // Mais ce n'est pas obligatoire
+    walk() {
+        console.log( 'Je marche discrètement ...' );
+    }
+}
+
+let bilbo = new Thief({
+    hp: 150,
+    armor: 150,
+    luck: 100
+});
+console.log( bilbo );
+bilbo.walk();
